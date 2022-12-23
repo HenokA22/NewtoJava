9b96b48137452c6244a50054c86467f7e45c53ee
Henok Assalif <109615606+HenokA22@users.noreply.github.com>
2022-12-22 17:20:26 -0800

    Add files via upload
    Improved the program by giving the user unlimited tries.

--- ChemistryMultipleChoice.java
+++ ChemistryMultipleChoice.java
@@ -1,47 +1,42 @@
 import java.util.Scanner;
-// made a multiple choice game that gives the user another try is wrong. Used while loop , else, if conditionals to create this program. 
+
 public class ChemistryMultipleChoice {
     public static void main(String[] args) {
-    String question = "What is correct molecular formula for Nitrate " + "A.) (NO2)^-1" + " B.) (NO3)^-1" + " C.) (NO)^-1";
-    String choiceone = "a";
-    String choicetwo = "b";
-    String choicethree = "c";
+        String question = "What is correct molecular formula for Nitrate " + "A.) (NO2)^-1" + " B.) (NO3)^-1" + " C.) (NO)^-1";
+        String choiceone = "a";
+        String choicetwo = "b";
+        String choicethree = "c";
 
-    String correctanswer = choicetwo;
+        String correctanswer = choicetwo;
 
-    System.out.println(question);
-    System.out.println(choiceone);
-    System.out.println(choicetwo);
-    System.out.println(choicethree);
-
-    Scanner scanner = new Scanner(System.in);
-    String input = scanner.next();
-
-    if (correctanswer.equals(input.toLowerCase())) {
-        System.out.println("Congrats you are correct");
-    }
-    else {
-        System.out.println("You are incorrect, try again");
         System.out.println(question);
-        boolean Secondchance = true;
-        while(Secondchance){
-            Scanner scanner1 = new Scanner(System.in);
-            String input2 = scanner1.next();
-            if(correctanswer.equals(input2.toLowerCase())) {
-                System.out.println("Congrats you are correct");
-                Secondchance = false;
-            }
-            else{
-                System.out.println("You are incorrect, the correct answer is " + choicetwo);
-                Secondchance = false;
+        System.out.println(choiceone);
+        System.out.println(choicetwo);
+        System.out.println(choicethree);
 
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        while (input != correctanswer) {
+            if (correctanswer.equals(input.toLowerCase())) {
+                System.out.println("Congrats you are correct!!");
+                input = correctanswer;
             }
+            else {
+
+
+                System.out.println("You are incorrect, try again");
+                System.out.println(question);
+                System.out.println(choiceone);
+                System.out.println(choicetwo);
+                System.out.println(choicethree);
+                input = scanner.next();
+            }
+
+        }
         }
 
     }
 
-
-}
-}
 
 
