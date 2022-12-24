483a95f3ee8a17e490c0dd7cce2d7cd9057719a2
Henok Assalif <109615606+HenokA22@users.noreply.github.com>
2022-12-23 21:49:38 -0800

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,10 @@
+//using a built-in functions this one specifically is a exponent tool
+
+public class Main2 {
+    public static void main(String[] args){
+        double result = Math.pow(2,5);
+        System.out.println(result);
+
+
+    }
+}
